79f0402d1698b49e5c219c43d30d5e8a0c76f6f3
dashboy <dashiellboyles@hotmail.com>
2023-10-13 11:11:08 +0100

    Raindrops falling

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+class RainDrop {
+  constructor() {
+    this.x = random(width)
+    this.y = random(-200, -100);
+    this.yspeed = 1
+  }
+  fall(){
+    this.y = this.y + this.yspeed
+  }
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+  show(){
+    stroke(138, 43, 226)
+    line(this.x, this.y, this.x, this.y + 10);
+  }
+}
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+const raindrops = new Array(500)
+
+function setup() {
+  console.log(raindrops)
+    createCanvas(window.innerWidth, window.innerHeight);
+    for (let i = 0; i < raindrops.length; i++) {
+      raindrops[i] = new RainDrop()
+    }
+}
+
+function draw() {
+    background(230, 230, 250);
+    for (let i = 0; i < raindrops.length; i++) {
+      raindrops[i].fall()
+      raindrops[i].show()
+    }
+}
